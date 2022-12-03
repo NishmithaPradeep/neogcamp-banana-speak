754aff51b8a4c46ae2be83b850c6f0ac677d6ed1
Nishmitha P <nishupradeep98@gmail.com>
2022-12-04 00:56:05 +0530

    docs - js file added

---
+++ app.js
@@ -0,0 +1,12 @@
+var btnTranslate = document.querySelector("#btn-translate");
+var txtInput = document.querySelector("#txt-input");
+var outputDiv = document.querySelector("#output");
+
+
+
+function clickHandler(){
+    console.log("clicked");
+    console.log("input", txtInput.value);
+}
+
+btnTranslate.addEventListener("click", clickHandler)
